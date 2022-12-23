d2a5a61705b5367f1b4e9de5a76f2b70e5df007a
LAKSHMI C K <lakshmi@glynk.com>
2022-12-23 14:34:08 +0530

    Model changes

--- IRCTC/models.py
+++ IRCTC/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    FirstName = models.CharField(max_length=100)
+    LastName = models.CharField(max_length=100)
+    gender = models.CharField(choices=['MALE', 'FEMALE', 'DONT DISCLOSE'])
+    username = models.CharField(max_length=16, unique=True)
+    password = models.CharField(max_length=16)
+    dob = models.DateField()
+    email = models.EmailField()
+    nationality = models.CharField(choices=['INDIAN', 'OTHERS'])
+
+class Trains(models.model):
+    train_no = models.IntegerField(primary_key=True)
+    date = models.DateField()
+    seats_AC = models.IntegerField()
+    seats_Sleeper = models.IntegerField()
+
+class Bookings(models.Model):
+    id = models.IntegerField(primary_key=True)
+    user_id = models.ForeignKey(User)
+    train = models.ForeignKey(Trains)
+    status = models.CharField(choices=['CONFIRMED', 'CANCELLED'])
+    
+
+
+
+
