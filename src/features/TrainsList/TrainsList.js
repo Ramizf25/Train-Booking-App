95243db93e27c86960bef4070cf251c0a775ca2f
LAKSHMI C K <lakshmi@glynk.com>
2022-12-27 15:31:36 +0530

    Filter feature

--- src/features/TrainsList/TrainsList.js
+++ src/features/TrainsList/TrainsList.js
@@ -1,15 +1,44 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchTrains } from './TrainsListSlice'
+import { from, to, date, general, quota } from './TrainsListSlice'
 
 function TrainsList() {
-    const trains_list = useSelector(state => {return state.TrainsList.trains});
+    //const trainsList = useSelector(state => {return state.TrainsList.trains});
+    const searchParams = useSelector(state => {return state.TrainsList.searchParams})
     const dispatch = useDispatch();
-    trains_list.forEach(element => {
-        console.log(element)
-    });
     return (
       <>
-      <button onClick={() => dispatch(fetchTrains())}>trains</button>
+        <h3>BOOK TICKET</h3>
+        <div>
+            <input list='Cities' placeholder='From' onChange={(event) => dispatch(from(event.target.value))}></input>
+            <input list='Cities' placeholder='To' onChange={(event) => dispatch(to(event.target.value))}></input>
+            <datalist id='Cities'>
+                <option value="BANGALORE"/>
+                <option value="MUMBAI"/>
+                <option value="CHENNAI"/>
+                <option value="CALCUTTA"/>
+                <option value="DELHI"/>
+            </datalist>
+        </div><br/>
+        <div>
+            <label>DD/MM/YYYY:
+                <input type="date" placeholder='Date' onChange={(event) => dispatch(date(event.target.value))}></input>
+            </label>
+        </div><br/>
+        <div>
+            <input list='Classes' placeholder='All Classes' onChange={(event) => dispatch(general(event.target.value))}></input>
+            <datalist id='Classes'>
+                <option value="All Classes"/>
+                <option value="Sleeper"/>
+                <option value="AC"/>
+            </datalist>
+            <input list='Quota' placeholder='General' onChange={(event) => dispatch(quota(event.target.value))}></input>
+            <datalist id='Quota'>
+                <option value="General"/>
+                <option value="Senior Citizen"/>
+            </datalist>
+        </div><br/>
+        <button onClick={() => dispatch(fetchTrains(searchParams))}>Search</button>
       </>
     )
   }
--- src/features/TrainsList/TrainsListSlice.js
+++ src/features/TrainsList/TrainsListSlice.js
@@ -2,40 +2,65 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from "axios";
 
 const client = axios.create({
-   baseURL: "http://127.0.0.1:8000/api/trains/" 
+   baseURL: "http://127.0.0.1:8000/"
 });
 
 
-export const fetchTrains = createAsyncThunk('/trains/list', async () => {
-  const response = await client.get('http://127.0.0.1:8000/api/trains/');
-  return response.data()
+export const fetchTrains = createAsyncThunk('/trains/list', async (searchParams) => {
+  const train_name = searchParams.from.slice(0,3)+'-'+searchParams.to.slice(0,3);
+  const search_api = '/api/trains/?train_name='+train_name+'&date='+searchParams.date;
+  const response = await client.get(search_api);
+  console.log(response)
+  return response.data
 })
 
 
 const TrainsSlice = createSlice({
   name: "Trains",
   initialState: {
+    searchParams: {
+      from:'',
+      to: '',
+      date: '',
+      general: '',
+      quota: ''
+    },
     trains: [],
     status: 'idle',
     error: null
   },
-  reducers: {},
+  reducers: {
+    from: (state, action) => {
+      state.searchParams.from = action.payload
+    },
+    to: (state, action) => {
+      state.searchParams.to = action.payload
+    },
+    date: (state, action) => {
+      state.searchParams.date = action.payload
+    },
+    general: (state, action) => {
+      state.searchParams.general = action.payload
+    },
+    quota: (state, action) => {
+      state.searchParams.quota = action.payload
+    }
+  },
   extraReducers: (builder) => {
     builder.addCase(fetchTrains.fulfilled, (state, action) => {
       state.status = 'succeeded'
       action.payload.forEach(train => {
         state.trains.push(train)
       });
-      //state.trains.push(action.payload)
-      })
+    })
     builder.addCase(fetchTrains.rejected, (state, action) => {
       state.status = 'failed'
       state.error = action.payload
-      })
+    })
     builder.addCase(fetchTrains.pending, (state, action) => {
       state.status = 'idle'
-      })
+    })
   }
 })
-
+export const {from, to, date, general, quota} = TrainsSlice.actions
 export default TrainsSlice.reducer
