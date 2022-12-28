56fe0ecd18bc0a573cc582e4f7103760db1ebf49
LAKSHMI C K <lakshmi@glynk.com>
2022-12-28 12:00:47 +0530

    Search results table

--- src/features/TrainsList/TrainsList.js
+++ src/features/TrainsList/TrainsList.js
@@ -3,19 +3,32 @@ import { useSelector } from 'react-redux'
 function TrainsList() {
     const trainsList = useSelector(state => {return state.TrainsList.trains});
     const listItems = trainsList.map(train =>
-        <li key={train.id}>
-          {"name: "}{train.train_name}{" ,"}   
-          {"number: "}{train.train_no }{" ,"}  
-          {"date: "}{train.date}{" ,"}  
-          {"seats AC: "}{train.seats_AC}{" ,"}  
-          {"seats sleeper: "}{train.seats_Sleeper}
-        </li>
+        <><tr key={train.id}>
+            <td>{train.train_name}</td>
+            <td>{train.train_no}</td>
+            <td>{train.date}</td>
+            <td><button>{train.seats_AC}</button></td>
+            <td><button>{train.seats_Sleeper}</button></td>
+            <td><button disabled>Book Now</button></td>
+        </tr></>
       );
         
     return (
         <>
             <h3>Search Results</h3>
-            <ul>{listItems}</ul>
+            <p>{trainsList.length} Results</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>   
+                        <th>Number</th> 
+                        <th>Date</th>
+                        <th>AC seats</th> 
+                        <th>Sleeper seats</th>
+                    </tr>
+                </thead>
+                <tbody>{listItems}</tbody>
+            </table>
         </>
         
     )
