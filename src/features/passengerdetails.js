5667dc05b5a299e071fa762766655c87f0a5826d
ramizf25 <ramiz9119@gmail.com>
2022-12-28 12:19:21 +0530

    add button added

--- src/features/passengerdetails.js
+++ src/features/passengerdetails.js
@@ -5,15 +5,17 @@ const api = axios.create({
     baseURL: "http://127.0.0.1:8000/api/passenger-details/" 
  });
 
+ const initialState =[{name:"",age: "", gender: '',nationality: '', preference: ''}];
 function Passengerdetails(){
-    const [data, setData]= useState({
-        name:"",age: "", gender: '',nationality: '', preference: ''
-    })
+    const [data, setData]= useState(initialState);
     console.log(data)
     function submit(event){
-        //event.preventDefault()
-        axios.post("http://127.0.0.1:8000/api/passenger-details/",data).then(function (response) {
-            console.log(response);})
+        
+        event.preventDefault()
+        axios.post("http://127.0.0.1:8000/api/passenger-details/",data.data).then(function (response) {
+            console.log(response);}).catch(function (error) {
+                console.log(error);
+              });
     }
     // createDetails = async () => {
     //     let res = await api.post('/',{ "name": 'Ramiz', "age": "30", "gender": 'male',
@@ -27,7 +29,11 @@ function Passengerdetails(){
             <input type={"text"} placeholder="Gender" onChange={event => setData({...data, gender: event.target.value })}/>
             <input type={"text"} placeholder="Nationality" onChange={event => setData({...data, nationality: event.target.value })}/>
             <input type={"text"} placeholder="Preference" onChange={event => setData({...data, preference: event.target.value })}/>
-            <button type="submit" >continue</button>
+            <button>Add</button>
+            <button type="submit">continue</button>
+            <h1>{data.name}{" "}{data.age}{" "}{data.gender}{" "}{data.nationality}{" "}{data.preference}</h1>
+
+            <p></p>
 
         </form>
     )
