a5a9daeb4a1648625771ce3fa57af855b9b0da3c
Anugrah Adams <anugrah.adams@gmail.com>
2022-12-29 18:19:25 +0530

    Fixing bug, don't use Form tag

--- src/features/Passenger/passengerdetails.js
+++ src/features/Passenger/passengerdetails.js
@@ -1,59 +1,71 @@
 import axios from "axios";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './style.css'
 const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/passenger-details/" 
- });
+    baseURL: "http://127.0.0.1:8000/api/passenger-details/"
+});
 
 
-function Passengerdetails(){
-    const initialState =[{name:"",age: "", gender: '',nationality: '', preference: ''}];
-    const [data, setData]= useState(initialState);
+function Passengerdetails() {
+    const initialState = [{ name: "", age: "", gender: '', nationality: '', preference: '' }];
+    const [data, setData] = useState(initialState);
+
+    useEffect(() => {
+        console.log("DATA MUTATED", data);
+    }, [data]);
+
     console.log(data)
-    function submit(event){
-        
-        event.preventDefault()
-        axios.post("http://127.0.0.1:8000/api/passenger-details/",data.data).then(function (response) {
-            console.log(response);}).catch(function (error) {
-                console.log(error);
-              });
+
+    function submit(event) {
+        axios.post("http://127.0.0.1:8000/api/passenger-details/", data.data).then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+            console.log(error);
+        });
     }
- 
-    function addRow(){ 
-        setData((d) => {console.log("data---x",d,data,initialState); return [...d,{...initialState[0]}]})
+
+    function addRow(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log("THIS IS THE EVENT PAYLOAD", event.isDefaultPrevented(), event.isPropagationStopped());
+        setData((d) => { console.log("data---x", d, data, [...d, { ...initialState[0] }]); return [...d, { ...initialState[0] }] })
     }
-    return(
-        
-        <form className="form" >
-        <div className="form">
-            {data.map((passenger,index)=>{ 
-            return(
-            <div key = {"passenger-"+index}>
-            <input type={"text"} placeholder="Name" value={passenger.name} 
-            onChange={(event)=> setData((data) => {data[index].name = event.target.value;
-                console.log("======");
+
+    function reactToChange(event, property, index) {
+        setData((data) => {
+            data[index][property] = event.target.value;
             return [...data]
-            })}/>
-            {/* <input type={"text"} placeholder="Age"  />
-            <input type={"text"} placeholder="Gender" />
-            <input type={"text"} placeholder="Nationality" />
-            <input type={"text"} placeholder="Preference" /> */}
-            </div>   
-)
-            
-            })}
-               
-        </div>  
-            <div className="button"><button onClick={addRow }>Add</button></div>
+        })
+    }
 
-            
-            {/* <button type="submit">continue</button> */}
-            {/* <h1>{data.name}{" "}{data.age}{" "}{data.gender}{" "}{data.nationality}{" "}{data.preference}</h1> */}
+    return (
+        <div>
+            <div className="form">
+                {
+                    data.map((passenger, index) => {
+                        return (
+                            <div key={"passenger-" + index}>
+                                <input type={"text"} placeholder="Name" value={passenger.name}
+                                    onChange={(event) => { reactToChange(event, 'name', index) }} />
+                                <input type={"text"} placeholder="Age" value={passenger.age}
+                                    onChange={(event) => { reactToChange(event, 'age', index) }} />
+                                <input type={"text"} placeholder="Gender" value={passenger.gender}
+                                    onChange={(event) => { reactToChange(event, 'gender', index) }} />
+                                <input type={"text"} placeholder="Preference" value={passenger.preference}
+                                    onChange={(event) => { reactToChange(event, 'preference', index) }} />
+                                <input type={"text"} placeholder="Nationality" value={passenger.nationality}
+                                    onChange={(event) => { reactToChange(event, 'nationality', index) }} />
+                            </div>
+                        )
 
-            <p></p>
-
-        </form>
-        
+                    })
+                }
+            </div>
+            <div className="button">
+                <button onClickCapture={addRow}>Add</button>
+                <button onClickCapture={submit}>Continue</button>
+            </div>
+        </div>
     )
 
 }
