57db9ccab4f5444a940c2b791645ac750c362cea
ramizf25 <ramiz9119@gmail.com>
2023-01-02 11:35:27 +0530

    Passenger page integrated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import SearchTrain from './features/TrainsList/SearchTrain'
 import ContactUs from './features/help/help'
 import About from './features/About/about'
 import TrainsList from './features/TrainsList/TrainsList'
+import Passengerdetails from './features/Passenger/passengerdetails'
 
 function App() {
   return(
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/"  element={<SearchTrain/>}/>
         <Route exact path="/about" element={<About/>}/>
         <Route exact path='/trains/list/' element={<TrainsList/>}/>
+        <Route exact path="/passenger/" element={<Passengerdetails/>}/>
       </Routes>
     </Router>
   </>
